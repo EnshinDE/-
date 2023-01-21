adeb630867c560703143dafbdfe59a67fb0526d4
EnshinDE <give.kit@mail.ru>
2023-01-21 22:41:56 +1000

    Добавил задания

--- Task3/Task3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Functions/Functions.h"
-
-int main()
-{
-    std::cout << encodeToBase64("A") << std::endl;
-    std::cout << decodeFromBase64("QQ==") << std::endl;
-    return 0;
-}
--- Task3/tests.cpp
+++
@@ -1,6 +0,0 @@
-#include "Functions/Functions.h"
-#include "gtest/gtest.h"
-TEST(ReadLineTest, ReadLine)
-{
-EXPECT_EQ(decodeFromBase64("QQ=="),"A");
-}
